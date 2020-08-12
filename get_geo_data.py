c4253253022f50397540f6098420e0f21cad8224
Rohit Saha <55949623+rohitmadrileno15@users.noreply.github.com>
2020-08-12 20:50:18 +0530

    Update get_geo_data.py

--- get_geo_data.py
+++ get_geo_data.py
@@ -1,5 +1,5 @@
 
-import geopy.distance
+from geopy.distance import geodesic 
 
 def geo_data_of_distance(current_lat , current_long , want_lat , want_long):
 
@@ -7,6 +7,6 @@ def geo_data_of_distance(current_lat , current_long , want_lat , want_long):
     coords_1 = (current_lat , current_long )
     coords_2 = (want_lat , want_long)
 
-    distance =  (geopy.distance.vincenty(coords_1, coords_2).km)
+    distance =  (geodesic(coords_1 , coords_2).km)
 
     return distance
